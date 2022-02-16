71f70853365f2642c62c3b1d0350efe65efb53ca
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 19:19:59 +0530

    Create date2.py

---
+++ date2.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)	
