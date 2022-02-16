42b49000b458b34b173061211655da023da6550e
Valentine <valentineinfantinejohn@gmail.com>
2022-02-17 01:16:28 +0530

    Create PythonFunction.py

---
+++ PythonFunction.py
@@ -0,0 +1,17 @@
+#Example
+def my_function():
+  print("Hello from a function")
+
+#Example
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#Example
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
