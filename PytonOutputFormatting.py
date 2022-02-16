219fa89714f4b76c3e4fd651ee2962311edda5d4
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 18:57:28 +0530

    Create PytonOutputFormatting.py

---
+++ PytonOutputFormatting.py
@@ -0,0 +1,11 @@
+print('This sentence is output to the screen')
+
+a = 5
+print('The value of a is', a)
+
+print(1, 2, 3, 4)
+print(1, 2, 3, 4, sep='*')
+print(1, 2, 3, 4, sep='#', end='&')
+
+print('I love {0} and {1}'.format('bread','butter'))
+print('I love {1} and {0}'.format('bread','butter'))
