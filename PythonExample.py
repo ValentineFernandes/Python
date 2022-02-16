e43afde3b56e22e263ecadd6f4b7c7bd64889cbb
Valentine <valentineinfantinejohn@gmail.com>
2022-02-17 00:55:00 +0530

    Create PythonExample.py

---
+++ PythonExample.py
@@ -0,0 +1,41 @@
+#Example
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Example
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#Example
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+#Example
+#This is a comment.
+print("Hello, World!")
+
+#Example
+print("Hello, World!") #This is a comment
+
+#Example
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Example
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#Example
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
