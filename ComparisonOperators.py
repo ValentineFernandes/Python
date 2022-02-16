8b694127061be771b490cbcb3a1f422d76de4856
Valentine <valentineinfantinejohn@gmail.com>
2022-02-16 18:52:59 +0530

    Create ComparisonOperators.py

---
+++ ComparisonOperators.py
@@ -0,0 +1,20 @@
+x = 10
+y = 12
+
+# Output: x > y is False
+print('x > y is',x>y)
+
+# Output: x < y is True
+print('x < y is',x<y)
+
+# Output: x == y is False
+print('x == y is',x==y)
+
+# Output: x != y is True
+print('x != y is',x!=y)
+
+# Output: x >= y is False
+print('x >= y is',x>=y)
+
+# Output: x <= y is True
+print('x <= y is',x<=y)
