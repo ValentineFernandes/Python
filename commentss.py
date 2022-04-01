6bb71c7a0c04a684822d022a3b8eded94200a7f1
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-03-31 22:44:03 -0800

    Create commentss.py

---
+++ commentss.py
@@ -0,0 +1,18 @@
+#Comment in Python.
+print("Hello, World!")
+
+''' Comment in Python '''
+
+print("Comment") #Comment in Python
+
+#Comment in Python
+#with
+#more than one line
+print("Comment!")
+
+"""
+Comment
+written in
+more than one line
+"""
+print("Comment''' ")
