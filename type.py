7a6fde9337a7ce34032a55a59e6f108af33b1b7f
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-02 01:47:08 -0800

    Create type.py

---
+++ type.py
@@ -0,0 +1,23 @@
+x = int(1)   
+y = int(2.8) 
+z = int("3") 
+
+a = float(1)     
+b = float(2.8)   
+c = float("3")   
+d = float("4.2") 
+
+e = str("s1") 
+f = str(2)    
+g = str(3.0)  
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(x)
+print(y)
+print(z)
