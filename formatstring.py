85b8149c4a8bb0f3751dda177b84c1b81d04b324
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-02 23:39:15 -0800

    Create formatstring.py

---
+++ formatstring.py
@@ -0,0 +1,11 @@
+quantity = 2
+items = 500
+price = 50.50
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, items, price))
+
+quantity = 5
+items = 600
+price = 100.50
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, items, price))
