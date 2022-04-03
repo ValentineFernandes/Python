30e7b61e71ef8f2b02a565adb76350e7ee9fa9ae
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-02 23:41:45 -0800

    Create escapesequence.py

---
+++ escapesequence.py
@@ -0,0 +1,14 @@
+SingleQuote = "The humans are \'Inhumans\' sometimes."
+print(SingleQuote)
+
+Backslash = "This will insert one \\ (backslash)."
+print(Backslash) 
+
+NewLine = "Hello\nWorld!"
+print(NewLine) 
+
+CarriageReturn = "Hello\rWorld!"
+print(CarriageReturn) 
+
+AnSpace = "So\tSpace!"
+print(AnSpace)
