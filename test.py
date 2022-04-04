f5589aeab7940be7d8368117c3144d3b6c85ee89
Valentine Fernandes <valentineinfantinejohn@gmail.com>
2022-04-03 23:03:44 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+x = "Hello World"
+print(len(x))
+
+txt = "Hello World"
+y = txt[0]
+
+txt = "Hello World"
+z = txt[2:5]
+
+txt = " Hello World "
+a = txt.strip()
+
+txt = "Hello World"
+b = txt.upper()
+
+txt = "Hello World"
+c = txt.lower()
+
+txt = "Hello World"
+d = txt.replace("H", "J")
+
+age = 25
+txt = "My name is Joe, and I am {}"
+print(txt.format(age))
